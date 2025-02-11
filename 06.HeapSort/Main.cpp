b4d8b79094bfe2fa6d20b1b77745be335f304095
imitme_unrealPU <37390101+imitme@users.noreply.github.com>
2025-02-11 11:49:44 +0900

    005. algorithm - HeapSort - 1 힙만들기

--- 06.HeapSort/Main.cpp
+++ 06.HeapSort/Main.cpp
@@ -3,11 +3,54 @@
 
 // 힙 만들기 (Heapify) 함수.
 void Heapify(std::vector<int>& array,int length,int ix)
-{}
+{
+	while(true)
+	{
+		//힙 구성을 위한 인덱스 변수
+		int largest = ix;
+
+		//자손 인덱트
+		int left = 2*ix+1;
+		int right = 2*ix+2;
+
+		if(left < length
+			&& array[left] > array[largest]
+			)
+		{
+			largest = left;
+		}
+
+		if(right < length
+			&& array[right] > array[largest]
+			)
+		{
+			largest = right;
+		}
+
+		if(largest == ix)	//변동사항이 없는경우
+		{
+			break;
+		}
+
+		std::swap(array[ix],array[largest]);
+		ix = largest;
+	}
+}
 
 // 힙 정렬 함수 (HeapSort).
 void HeapSort(std::vector<int>& array)
-{}
+{
+	//전체 크기
+	int length = (int)array.size();
+
+	//힙 구성
+	for(int ix = length /2 - 1; ix >= 0 ; ix--)
+	{
+		Heapify(array,length,ix);
+	}
+
+	std::cin.get();
+}
 
 // 벡터 출력 함수.
 void PrintArray(std::vector<int>& array)
