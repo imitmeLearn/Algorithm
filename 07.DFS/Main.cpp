2ed79b45fc5ef1679abac2b1f0642aeab85f1b80
imitme_unrealPU <37390101+imitme@users.noreply.github.com>
2025-02-13 14:48:36 +0900

    005. algorithm - 7.DFS - 미로탐색1

---
+++ 07.DFS/Main.cpp
@@ -0,0 +1,137 @@
+﻿#define _CRTDBG_MAP_ALLOC
+#include <stdlib.h>
+#include <crtdbg.h>
+
+#include <iostream>
+
+#include <vector>
+#include <stack>
+
+//미로 탐색
+static struct Location2D
+{
+	Location2D(int row=0,int col =0)
+		:row(row),col(col)
+	{}
+
+	int row;
+	int col;
+};
+//맵 배열
+int mapSize =0;
+std::vector<std::vector<char>> map
+{
+	{'1','1','1','1','1','1'},
+	{'e','0','1','0','0','1'},
+	{'1','0','0','0','1','1'},
+	{'1','0','1','0','1','1'},
+	{'1','0','1','0','0','x'},
+	{'1','1','1','1','1','1'}
+};
+
+//이동 가능 여부 판단 함수.
+bool IsValidLocation(int row,int col)
+{
+	if(row < 0 || row >= mapSize
+		||col < 0 || col >= mapSize)
+	{
+		return false;
+	}
+	return map[row][col] =='0' || map[row][col] =='x';
+}
+
+//시작지점 검색 함수
+void FindStartLocation(int& row,int& col)
+{
+	for(int ix = 0; ix < mapSize; ix++)
+	{
+		for(int jx = 0; jx < mapSize; jx++)
+		{
+			//맵출력
+			std::cout << map[ix][jx] << " ";
+
+			if(map[ix][jx] == 'e')
+			{
+				row = ix;
+				col = jx;
+				//return;
+			}
+		}
+
+		//개행
+		std::cout<<"\n";
+	}
+}
+
+//미로탐색 함수
+void EscapeMaze()
+{
+	//맵 크기 확인
+	mapSize =(int)map.size();
+
+	int row = 0;
+	int col = 0;
+
+	//시작위치찾기
+	FindStartLocation(row,col);
+
+	//스택선언
+	std::stack<Location2D> stack;
+
+	stack.push(Location2D(row,col));
+
+	//미로탐색
+	while(!stack.empty())
+	{
+		//현재위치 반환
+		Location2D current = stack.top();
+		stack.pop();
+
+		//탐색 위치 출력
+		std::cout<<"("<< current.row << ", " << current.col <<") ";
+
+		//값 저장
+		row = current.row;
+		col = current.col;
+
+		//탈출 조건 확인
+		if(map[row][col] == 'x')
+		{
+			std::cout<<"\n 미로 탐색 성공 \n";
+			return;
+		}
+
+		//탐색 진행.
+		//방문한 현재 위치는 재방문 방지를 위해 표시.
+		map[row][col] = '.';
+
+		if(IsValidLocation(current.row -1,current.col))
+		{
+			stack.push(Location2D(current.row -1,current.col));
+		}
+		if(IsValidLocation(current.row +1,current.col))
+		{
+			stack.push(Location2D(current.row +1,current.col));
+		}
+		if(IsValidLocation(current.row,current.col-1))
+		{
+			stack.push(Location2D(current.row,current.col-1));
+		}
+		if(IsValidLocation(current.row,current.col+1))
+		{
+			stack.push(Location2D(current.row,current.col+1));
+		}
+	}
+
+	std::cout<<"\n 미로 탐색 실패 \n";
+}
+
+int main()
+{
+	_CrtSetDbgFlag (_CRTDBG_ALLOC_MEM_DF | _CRTDBG_LEAK_CHECK_DF);
+
+	std::cout<<"\n"<<"//미로탐색"<<"\n";
+	EscapeMaze();
+
+	std::cin.get();
+}
