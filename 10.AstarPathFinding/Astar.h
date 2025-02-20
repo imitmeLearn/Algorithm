aa67ff592a258682834163f169376957371e39a6
imitme_unrealPU <37390101+imitme@users.noreply.github.com>
2025-02-20 10:53:21 +0900

    10.AStarPathFinding - 0. settings

---
+++ 10.AstarPathFinding/Astar.h
@@ -0,0 +1,65 @@
+﻿#pragma once
+#include <vector>
+template <typename T>
+
+//포인터 삭제 함수
+void SafeDelete(T*& t)
+{
+	if(t)
+	{
+		delete t;
+		t=nullptr;
+	}
+}
+
+class Node;	//전방선언
+class Astar //A* 길찾기 기능 처리 클래스.
+{
+	struct Direction
+	{
+		int x = 0;
+		int y = 0;
+
+		float cost = 0.f;
+	};
+
+public:
+	Astar();
+	~Astar() = default;
+
+	//경로 검색 함수.
+	std::vector<Node*> FindPath(
+		Node* startNode
+		,Node* goalNode
+		,const std::vector<std::vector<int>>& grid
+	);
+
+	//그리드 출력 함수. : 맵 보여주는 함수
+	void DisplayGridWithPath(
+		const std::vector<std::vector<int>>& grid
+		,const std::vector<Node*>& path
+	);
+
+private:	//처리관련 함수는 private 로!
+	//탐색을 완료한 후에 최적 경로를 반환하는 함수. : //목표노드로 부터 부모 노드를 역추적해 구함
+	std::vector<Node*> ConstructPath(Node* goalNode);
+
+	//경로 계산 비용계산 함수. 휴리스트 계산 함수.
+	float CalculateHeuristic(Node* currentNode,Node* goalNode);
+
+	//탐색 하려는 곳이 그리드(맵) 안에 있는지, 실제 게임엔진도, 네비게이션 처리 시, 이동 가능한 맵인지, 경로계산 불가하게 하는 부분 파악하는 함수.
+	bool isInRange(int x,int y,const std::vector<std::vector<int>>& grid);	//2차원 배열인데, 참조니까 또 복사하지 않지만, 그래도 1차원으로 바쑬 필요 있음
+
+	//이미 방문한 위치인지 확인하는 함수
+	bool HasVisited(int x,int y,float gCost);
+
+	//탐색하려는 위치가 목표 위치인지 확인하는 함수
+	bool IsDestination(const Node* node);
+
+private:
+	std::vector<Node*> openList;	//열린 리스트
+	std::vector<Node*> closedList;	//닫힌 리스트
+
+	Node* startNode = nullptr;		//시작 노드
+	Node* goalNode = nullptr;		//목표 노드
+};
---
+++ 10.AstarPathFinding/Node.h
@@ -0,0 +1,40 @@
+﻿#pragma once
+
+#include <iostream>
+
+struct Position
+{
+	Position(int = 0,int y=0)
+	{}
+	int x;
+	int y;
+};
+
+class Node
+{
+public:
+	Node(int x,int y,Node* parent = nullptr): position(x,y),parent(parent){}
+	~Node() = default;
+
+	//연산자 오버로딩. //두 노드 같은지 비교하려면, 포인터라면, 주소비교, 스택이면, 값을 비교
+	Position operator-(const Node& other)
+	{
+		return Position(position.x - other.position.x,position.y - other.position.y);
+	}
+
+	bool operator==(const Node& other) const
+	{
+		return position.x == other.position.x && position.y == other.position.y;
+	}
+
+public:
+	Position position;	//노드의 위치
+
+	float gCost =0.f;	//현재 노드에서 이동 노드까지의 비용
+	float hCost =0.f;	//이동 노드에서 목표 노드까지의 비용 ( 추청치, 휴리스틱)
+	float fCost =0.f;	//두 비용을 합친 비용 (최종 Cost)
+
+	Node* parent = nullptr;	//링크 필드(부모 노드)
+
+private:
+};
