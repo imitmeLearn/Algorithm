ff46ac2c6d30b323bf9f265b5745d8e043748a0e
imitme_unrealPU <37390101+imitme@users.noreply.github.com>
2025-02-21 10:24:51 +0900

    11.QuadTree - 0. settings

---
+++ 11.QuadTree/Main.cpp
@@ -0,0 +1,14 @@
+﻿#define _CRTDBG_MAP_ALLOC
+#include <stdlib.h>
+#include <crtdbg.h>
+
+#include <iostream>
+
+int main()
+{
+	_CrtSetDbgFlag (_CRTDBG_ALLOC_MEM_DF | _CRTDBG_LEAK_CHECK_DF);
+
+	std::cout<<"\n"<<"//"<<"\n";
+
+	std::cin.get();
+}
